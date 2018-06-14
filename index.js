131cef6dc1d3864195bcb1481b65dd31c2e843b9
David Anderson <matandersonlv@hotmail.com>
2018-06-14 21:32:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ function takeANumber(katzDeliLine, name) {
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
 
-function nowServing() {
+function nowServing(katzDeliLine, ) {
   
 }
